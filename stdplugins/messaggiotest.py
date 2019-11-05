27b17737b5e70e34cd11757b3a765a9a2f26801a
SaimonFiga <55721604+SaimonFiga@users.noreply.github.com>
2019-11-05 11:53:03 +0100

    Create messaggiotest.py

---
+++ stdplugins/messaggiotest.py
@@ -0,0 +1,11 @@
+from telethon import TelegramClient, events
+
+client = TelegramClient('anon', api_id, api_hash)
+
+@client.on(events.NewMessage)
+async def my_event_handler(event):
+    if 'hello' in event.raw_text:
+        await event.reply('hi!')
+
+client.start()
+client.run_until_disconnected()
